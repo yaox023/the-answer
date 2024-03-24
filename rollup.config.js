a3e712b0e562a635239e4da05429de6a6f0fb13f
yao <yaoyaox023@gmail.com>
2024-03-24 09:49:20 +0900

    convert to cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
-const pkg = require( './package.json' );
+const pkg = require('./package.json');
 
 export default {
-	entry: 'src/index.js',
-	moduleName: 'theAnswerToTheQuestionOfLifeTheUniverseAndEverything',
-	targets: [
-		{ dest: pkg.main, format: 'umd' },
-		{ dest: pkg.module, format: 'es' }
-	]
+  entry: 'src/index.js',
+  targets: [
+    { dest: pkg.main, format: 'cjs' },
+  ]
 };
